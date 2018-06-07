2ddff7ebf4a2c64c259bc33008a0b7dc2462130e
Sarah Edkins <edkins.sarah@gmail.com>
2018-06-07 07:45:56 -0400

    redraw worm and ground on color change

--- src/SimpleGame.js
+++ src/SimpleGame.js
@@ -18,17 +18,24 @@ class SimpleGame extends Component {
       height: window.innerHeight - 50,
       wormColor: 0,
       colors: ['#0095DD', '#ff9966', '#ccccff', '#ffccff'],
-      wormX: 50,
-      wormY: 50,
-      wormWidth: 20,
-      wormHeight: 10,
+      wormStartX: 60,
+      wormStartY: 50,
+      wormEndX: 110,
+      wormEndY: 50,
+      wormWidth: 50,
+      wormHeight: 23,
+      ctx: null,
+      groundXY: [],
     };
   }
 
   componentDidMount() {
-    this.initGame();
-    this.drawWorm();
-    this.drawGround();
+    this.setState({
+      ctx: this.canvasRef.current.getContext('2d'),
+    }, this.initGame);
+    // this.initGame();
+    // this.drawWorm();
+    // this.drawGround();
     window.addEventListener('resize', this.resizeCanvas);
     window.addEventListener('keydown', this.keyDownHandler);
   }
@@ -46,91 +53,87 @@ class SimpleGame extends Component {
   }
 
   keyDownHandler = (e) => {
-    if (e.keyCode == 67) {
+    if (e.keyCode == 67) { // letter 'c'
         this.setState({
           wormColor: (this.state.wormColor + 1) % this.state.colors.length,
-        }, this.drawWorm)
-    } else if (e.keyCode == 32 || e.keyCode == 13 || e.keyCode == 38 ) {
+        }, this.reDrawWorm)
+    } else if (e.keyCode == 32 || e.keyCode == 13 || e.keyCode == 38 ) { // spacebar, enter, up arrow
       console.log("jump worm!");
       this.setState({
-        wormY: 10,
+        wormStartY: 10,
       }, this.drawWorm);
     }
   }
 
   initGame() {
-    const ctx = this.canvasRef.current.getContext('2d');
+    const { ctx } = this.state;
     ctx.fillStyle = 'white';
     ctx.fillRect(0,0, this.state.width, this.state.height);
+    this.drawWorm();
+    // this.drawGround();
+    this.initGround();
+  }
+
+  // deletes existing worm before re-drawing
+  reDrawWorm() {
+    const { ctx } = this.state;
+    this.clearCanvas();
+    this.drawWormPath(ctx);
+    this.drawGround();
   }
 
   drawWorm() {
-    let x = this.state.wormX;
-    let y = this.state.wormY;
-    let dx = 2;
-    let dy = 0;
-    let start = 0;
-    const ctx = this.canvasRef.current.getContext('2d');
-    const update = () => {
-      x = x + dx;
-      y = y + dy;
-      this.drawWormPath(ctx, x, y);
-      if (start < this.state.wormWidth) {
-        start++;
-        setTimeout(() => update(), 10);
-      }
-    }
-    update();
+    const { ctx } = this.state;
+    this.drawWormPath(ctx);
   }
 
-  // dot
-  drawWormPath = (ctx, x, y) => {
-    // ctx.clearRect(50, 50, 20, 10);
+  drawWormPath = (ctx) => {
+    const { wormStartX, wormStartY, wormEndX, wormEndY } = this.state;
+    ctx.lineWidth = this.state.wormHeight;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = this.state.colors[this.state.wormColor];
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = this.state.colors[this.state.wormColor];
-    ctx.fill();
+    ctx.moveTo(wormStartX, wormStartY);
+    ctx.lineTo(wormEndX, wormEndY);
+    ctx.stroke();
     ctx.closePath();
   }
 
-  // // line
-  // drawWormPath2 = (ctx, x, y) => {
-  //   ctx.lineWidth = 15;
-  //   ctx.lineCap = "round";
-  //   ctx.strokeStyle = this.state.colors[this.state.wormColor];
-  //   ctx.beginPath();
-  //   ctx.moveTo(20, 30);
-  //   ctx.lineTo(60, 30);
-  //   ctx.stroke();
-  //   ctx.closePath();
-  // }
-
-  drawGround() {
-    let x = 50;
-    let y = 62;
-    let dx = 2;
-    let dy = 0;
-    let start = 0;
-    const ctx = this.canvasRef.current.getContext('2d');
-    const update = () => {
-      x = x + dx;
-      y = y + dy;
-      this.drawGroundPath(ctx, x, y);
-      if (start < this.state.width) {
-        start++;
-        setTimeout(() => update(), 10);
-      }
-    }
-    update();
+  clearCanvas = () => {
+    const { ctx, width, height } = this.state;
+    ctx.clearRect(0, 0, width, height);
   }
 
-  drawGroundPath(ctx, x, y) {
+  initGround() {
+    let x = 50;
+    let y = 62;
+    const dx = 2;
+    const dy = 0; // 0 = ground is flat
+    const groundXY = [{x, y}];
+    let start = 0;
+    while (start < this.state.width) {
+      start++;
+      x = x + dx;
+      y = y + dy;
+      groundXY.push({ x, y });
+    }
+    this.setState({
+      groundXY
+    }, this.drawGround);
+  }
+
+  drawGround() {
+    this.state.groundXY.forEach((groundPt) => {
+      this.drawGroundPath(groundPt.x, groundPt.y);
+    });
+  }
+
+  drawGroundPath(x, y) {
+    const { ctx } = this.state;
     ctx.beginPath();
     ctx.arc(x, y, 1, 0, Math.PI*2);
     ctx.fillStyle = "#000000";
     ctx.fill();
-    ctx.closePath();
-
     const randomNumber = Math.floor(Math.random() * 100);
     if (randomNumber % 10 === 0) {  // adds dirt to ground
       const randomDotDistance = Math.floor(Math.random() * 10);
@@ -138,14 +141,12 @@ class SimpleGame extends Component {
       ctx.arc(x, y+randomDotDistance, 1, 0, Math.PI*2);
       ctx.fillStyle = "#000000";
       ctx.fill();
-      ctx.closePath();
     } else if (randomNumber % 5 === 0) { // adds leafs on ground
       const randomHeight = Math.floor(Math.random() * 10);
       ctx.beginPath();
       ctx.arc(x, y-randomHeight, 3, 0, Math.PI*2);
       ctx.fillStyle = "#2eb82e";
       ctx.fill();
-      ctx.closePath();
     }
   }
 
