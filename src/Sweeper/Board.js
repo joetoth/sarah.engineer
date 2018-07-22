a105554da09aa8629e7ba7c3f3f0c12e2f0d3fbc
Sarah Edkins <edkins.sarah@gmail.com>
2018-07-22 08:55:04 -0400

    flag count

--- src/Sweeper/Board.js
+++ src/Sweeper/Board.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 
+const N = 11;
+const BOMB = "💣";
+const FLAG = "🚩";
+
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDoubleClick = this._handleDoubleClick.bind(this);
+  }
+
+  /*
+    single - add flag
+    single - remove flag
+    double - reveal value
+  */
+
+  _handleDoubleClick(event) {
+    this.props.doubleClick(event.target.id);
+  }
+
   clickHandler = (event) => {
-    this.props.updateBoard(event.target.id);
+    this.props.singleClick(event.target.id);
   }
 
   render() {
-    const { status } = this.props;
+    const { status, value, id, flag } = this.props;
     if (status === "default") {
       return (
         <div
           className="square blue"
           onClick={this.clickHandler}
-          id={this.props.id}
-        />
+          onDoubleClick={this.handleDoubleClick}
+          id={id}
+        >
+          {flag ? FLAG : null}
+        </div>
       );
     } else {
       return (
         <div
           className={`flex flex-align-items-center
-            flex-justify-content-center square blue dimmed`}
+            flex-justify-content-center square blue
+            ${value !== BOMB ? 'dimmed' : ''}`}
           onClick={this.clickHandler}
-          id={this.props.id}
+          onDoubleClick={this.handleDoubleClick}
+          id={id}
         >
-          {this.props.value}
+          {value !== 0 ? value : null}
         </div>
       );
     }
@@ -40,6 +64,23 @@ const boardMap = (rows, cols) => {
   return result;
 }
 
+const getRandomInteger = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const getBombLocations = (n) => {
+  const bombLocations = [];
+  while (bombLocations.length < n) {
+    const randomRow = getRandomInteger(n);
+    const randomCol = getRandomInteger(n);
+    const bombKey = `row${randomRow}col${randomCol}`;
+    if (!bombLocations.includes(bombKey)) {
+      bombLocations.push(bombKey);
+    }
+  }
+  return bombLocations;
+}
+
 const initSquareStatus = (squares) => {
   const squareStatus = {};
   squares.forEach(square => {
@@ -48,11 +89,55 @@ const initSquareStatus = (squares) => {
   return squareStatus;
 }
 
+// row10col10 => "row10" "col10"
+const parseKey = (key) => {
+  const split = key.split("col");
+  split[0] = split[0].split("row")[1];
+  return split;
+}
+
+const getKey = (x, y) => `row${x}col${y}`;
+
+const getValidKey = (n, X, Y) => {
+    if (X >= 0 && Y >= 0 && X < n && Y < n) {
+      return getKey(X, Y);
+    }
+    return null;
+}
+
+const getNeighbors = (n, x, y) => {
+  let neighbors = [];
+  neighbors.push(getValidKey(n, x-1, y-1));
+  neighbors.push(getValidKey(n, x-1, y));
+  neighbors.push(getValidKey(n, x-1, y+1));
+  neighbors.push(getValidKey(n, x, y-1));
+  neighbors.push(getValidKey(n, x, y+1));
+  neighbors.push(getValidKey(n, x+1, y-1));
+  neighbors.push(getValidKey(n, x+1, y));
+  neighbors.push(getValidKey(n, x+1, y+1));
+  neighbors = neighbors.filter(key => key);
+  return neighbors;
+}
+
+const getBombTouchCount = (squareValue, x, y) => {
+  const X = parseInt(x, 10);
+  const Y = parseInt(y, 10);
+  const neighbors = getNeighbors(N, X, Y);
+  return neighbors.filter(key => squareValue[key] === BOMB).length;
+}
+
 const initSquareValue = (squares) => {
   const squareValue = {};
-  // TODO - bomb/value distribution logic
-  squares.forEach(square => {
-    squareValue[square] = "1";
+  const bombLocations = getBombLocations(N);
+  bombLocations.forEach(bombLocation => {
+    squareValue[bombLocation] = BOMB;
+  });
+  squares.forEach(key => {
+    if (!squareValue[key] || squareValue[key] !== BOMB) {
+      const xy = parseKey(key);
+      const bombCount = getBombTouchCount(squareValue, xy[0], xy[1]);
+      squareValue[key] = bombCount;
+    }
   });
   return squareValue;
 }
@@ -65,7 +150,7 @@ const FaceIcon = ({ happy }) => (
 
 const ProgressChart = ({ bombsLeft, happy }) => (
   <div className="flex flex-align-items-center montserrat progress-chart">
-    <div className="pad-10">{bombsLeft}</div>
+    <div className="pad-10">{FLAG}{bombsLeft}</div>
     <FaceIcon happy={happy} className="pad-10" />
   </div>
 );
@@ -73,28 +158,31 @@ const ProgressChart = ({ bombsLeft, happy }) => (
 class Board extends Component {
   constructor() {
     super();
-    const rows = 11;
-    const cols = 11;
+    const rows = N;
+    const cols = N;
     const squares = boardMap(rows, cols);
     this.state = {
       rows,
       cols,
-      totalBombs: 10,
-      bombsRemaining: 10,
+      totalBombs: N,
+      flagsPlaced: 0,
       happy: true,
       squares,
       squareStatus: initSquareStatus(squares),
       squareValue: initSquareValue(squares),
+      squareFlagged: {},
     }
   }
 
-  squareUpdate = (id) => {
-    console.log("squareUpdate id", id);
+  revealSquare = (id) => {
+    this.setState({
+      flagsPlaced: this.state.flagsPlaced - 1,
+    })
     const status = this.state.squareStatus[id];
-    console.log('STATUS', status);
+    const value = this.state.squareValue[id];
     if (status === 'default') {
-      this.state.squareStatus[id]
       this.setState({
+        happy: value !== BOMB,
         squareStatus: {
           ...this.state.squareStatus,
           [id]: "reveal"
@@ -103,28 +191,65 @@ class Board extends Component {
     }
   }
 
+  updateFlagsPlaced = (id) => {
+    const flag = this.state.squareFlagged[id];
+      this.setState({
+        squareFlagged: {
+          ...this.state.squareFlagged,
+          [id]: !flag,
+        },
+      });
+  }
+
+  doNothing = () => {
+    return null;
+  }
+
+  resetGame = () => {
+    this.setState({
+      totalBombs: N,
+      flagsPlaced: 0,
+      happy: true,
+      squareStatus: initSquareStatus(this.state.squares),
+      squareValue: initSquareValue(this.state.squares),
+    })
+  }
+
+  getFlagCount = () => Object.keys(this.state.squareFlagged)
+    .filter(key => this.state.squareFlagged[key]).length;
+
   render() {
     const {
       happy,
-      bombsRemaining,
+      totalBombs,
+      flagsPlaced,
       squares,
       squareStatus,
       squareValue,
+      squareFlagged,
     } = this.state;
     return  (
         <div>
-          <ProgressChart happy={happy} bombsLeft={bombsRemaining} />
+          <ProgressChart happy={happy} bombsLeft={totalBombs - this.getFlagCount()} />
+
           <div className="flex board">
           { squares.map((square, i) => (
             <Square
               key={square}
-              updateBoard={this.squareUpdate}
+              doubleClick={this.state.happy ? this.revealSquare : this.doNothing}
+              singleClick={this.state.happy ? this.updateFlagsPlaced : this.doNothing}
               id={square}
               status={squareStatus[square]}
               value={squareValue[square]}
+              flag={squareFlagged[square]}
             />
           ))}
           </div>
+          { !this.state.happy ? (
+            <button onClick={this.resetGame}>
+              New Game
+            </button>) : null
+          }
         </div>
       );
   }
